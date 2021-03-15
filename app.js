222df1cab0895468d7e60fb34582ff6c50ecca0d
nbur4556 <nbur4556@gmail.com>
2021-03-14 20:11:44 -0500

    feat: create discord client

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello Discord Bot');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// Connect to Discord
+client.on('ready', () => {
+    console.log('client is ready...');
+});
+
+// Create event listener for update from github
+// On event, send message to github channel
+
+// Log in bot using token from https://discord.com/developers/applications
